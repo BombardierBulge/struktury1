6b8d61f78f2c9aa48020461801861a1c5ddcd5f6
BombardierBulge <z.bartosz.z@wp.pl>
2025-04-06 18:22:39 +0200

    feat: add time measurement functionality

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include "timer/timer.hpp"
 
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    Timer timer; // Utwórz obiekt Timer
+    timer.reset(); // Rozpocznij pomiar czasu
+    cout<< "Pomiar czasu w mikrosekundach: " << endl;
+    timer.stop(); // Zatrzymaj pomiar czasu
+    cout<< timer.measurement_micro()<<" \xE6s"<<std::endl;
+    timer.reset(); // Rozpocznij pomiar czasu
+    cout<< "Pomiar czasu w mikrosekundach: " << endl;
+    cout<< timer.measurement_stop(Timer::TimeUnit::Microseconds)<<" \xE6s"<<std::endl;
     return 0;
 }
