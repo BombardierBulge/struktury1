19104bce8a0f43137115fd11279235240a2ca53a
Wiciu85 <wiktor.kanoza@gmail.com>
2025-04-08 00:32:58 +0200

    Add files via upload

---
+++ Lista_jednokierunkowa/Lista_jednokierunkowa.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+struct Node {
+	int value;
+	Node* next;
+};
+
+struct List {
+	Node* head = nullptr;
+	Node* tail = nullptr;
+};
+
+void Addvaluefirst(List& list, int value);
+void Addvaluelast(List& list, int value);
+void Addvalue(List& list, int value, int index);
+void deletefirstvalue(List& list);
+void deletelastvalue(List& list);
+void deletevalue(List& list, int value);
+Node* search(List& list, int value);
+void PrintList(List& list);
+
+int main();
+
+
