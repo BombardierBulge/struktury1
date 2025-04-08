7b19aab27800339e48c7e86bbac014c16271b53e
BombardierBulge <z.bartosz.z@wp.pl>
2025-04-08 21:51:48 +0200

    fix: skill issue

--- main.cpp
+++ main.cpp
@@ -11,8 +11,8 @@ void createFile(int size) {
 	RandomNumberGenerator generator(1, 1000000, "losowe" + to_string(size) + ".txt");
 	generator.generateFile(size);
 }
-int fileSizes[] = {100, 1000};//, 50000, 100000, 200000, 500000, 1000000 };
-const int FilesNumber = 2;
+int fileSizes[] = { 10000 ,30000,50000,100000,300000,500000,1000000,3000000,5000000,10000000 };
+const int FilesNumber = 10;
 void createNFiles() {//Tworzy pliki o rozmiarach z fileSizes[]
 	int n = sizeof(fileSizes) / sizeof(fileSizes[0]);
 	for (int i = 0; i < n; i++) {
@@ -21,13 +21,12 @@ void createNFiles() {//Tworzy pliki o rozmiarach z fileSizes[]
 }
 void fillstructures(List* list[], ArrayList* array[]) {
 	for (int i = 0; i < FilesNumber; i++) {
-		for(int j = 0; j < fileSizes[i]; j++) {
 			loadFileToSinglyLinkedList("losowe" + to_string(fileSizes[i]) + ".txt", *list[i]);
 			loadFileToArrayList("losowe" + to_string(fileSizes[i]) + ".txt", *array[i]);
-		}
 	}
 }
 int main() {
+	cout<<"start"<<endl;
 	createNFiles();
    List* listy[FilesNumber];
    ArrayList* tablice[FilesNumber];
@@ -40,8 +39,8 @@ int main() {
    cout<<"pomiar czasu wczytu... \n";
    timer.reset();
    fillstructures(listy, tablice);
-   cout<<"czas wczytywania do tablicy: "<<timer.measurement_stop(Timer::TimeUnit::Seconds)<<"\n";
-   
+   cout<<"czas wczytywania do tablicy: "<<timer.measurement_stop(Timer::TimeUnit::Microseconds)<<"\n";
+   cout<<"index ostatni(5000000): "<<tablice[9]->get(9999999)<<endl;
    for (int i = 0; i < FilesNumber; i++) {
 	   delete listy[i];
 	   delete tablice[i];
